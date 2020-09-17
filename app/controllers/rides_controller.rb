4c366613e370d086dc8b4f9e7e1326f9610ad973
nabilhayet <nabil4457@yahoo.com>
2020-09-17 15:28:14 -0400

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,26 +2,25 @@ class RidesController < ApplicationController
     
     
     def create
-        @user = current_user
-        @ride = Ride.new(ride_params)
-        if @ride.valid?
-            @ride.save
-            @message = @ride.take_ride 
-            @user = @ride.user 
-            @user.save 
-            redirect_to user_path(@user)
-        end 
-     end 
+    #     @user = current_user
+    #     @ride = Ride.new(ride_params)
+    #     if @ride.valid?
+    #         @ride.save
+    #         @message = @ride.take_ride 
+    #         @user = @ride.user 
+    #         @user.save 
+    #         redirect_to user_path(@user)
+    #     end 
+    #  end 
         
 
-        # @ride = Ride.create(
-        #   :user_id => params[:user_id],
-        #   :attraction_id => params[:attraction_id]
-        # )
-        # @message = @ride.take_ride
-        # redirect_to user_path(@ride.user, :message => @message)
-    
-    end  
+        @ride = Ride.create(
+          :user_id => params[:user_id],
+          :attraction_id => params[:attraction_id]
+        )
+        @message = @ride.take_ride
+        redirect_to user_path(@ride.user, :message => @message)
+    end 
 
     private 
 
